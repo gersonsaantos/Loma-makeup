b8b04dfe715d35c10e1df62823fa2b62ae019bf2
gersonsaantos <147741035+gersonsaantos@users.noreply.github.com>
2024-07-26 15:55:51 -0300

    Add files via upload

---
+++ src/JS/index.js
@@ -0,0 +1,42 @@
+//Js da Pagina 4
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+    },
+    pagination: {
+    el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 3000, // Tempo em milissegundos entre os slides
+        disableOnInteraction: false, // Continua a deslizar automaticamente após interações do usuário
+    },
+    loop: true, 
+    speed: 1000,
+});
+
+//Js da pagina 3
+
+var swiper2 = new Swiper('.mySwiper2', {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000, // Tempo em milissegundos entre os slides
+        disableOnInteraction: false, // Continua a deslizar automaticamente após interações do usuário
+    },
+    loop: true, 
+    speed: 3000,
+    
+});
